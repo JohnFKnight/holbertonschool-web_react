48c406ad221955e941836e58ca6ad029ea92be39
jfkquality <jfkquality@gmail.com>
2021-02-10 07:02:22 +0000

    589. t.0. [Bugfix]. Change for-in to forEach to for-of.

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -1,3 +1,4 @@
+'use strict';
 interface Student {
   firstName: string;
   lastNname: string;
@@ -19,30 +20,17 @@ let student2: Student = {
 }
 
 const studentsList: any = [student1, student2];
-
-function generateTableHead(table: any, data: any) {
-	 let thead = table.createTHead(table);
-	 let row = thead.insertRow();
-	 for(let key of data) {
-	 	 let th = document.createElement('th');
-		 let text = document.createTextNode(key);
-		 th.appendChild(text);
-		 row.appendChid(th);
-	 }
-}
-
-function generateTable(table: any, data: any) {
-	 for (let element of data) {
-	     let row = table.insertRow();
-	     for (let key in element) {
-	     	 let cell = row.insertCell();
-		 let text  = document.createTextNode(element[key]);
-		 cell.appendChild(text)
-	     }
-	 }
-}
-
 let table = document.createElement('table');
 let data = Object.keys(studentsList[0]);
-generateTable(table, studentsList);
-generateTableHead(table,data);
+
+document.body.appendChild(table);
+
+for (let element of data) {
+    console.log('ELEMENT: ', element);
+    let row = table.insertRow();
+    for(let val of element) {
+    	let td = row.insertCell();
+	let text = document.createTextNode(val);
+	td.appendChild(text);
+     }
+}
