4bab1b6837a3f4f6f917bfd0c9111b3afd0f1f13
jfkquality <jfkquality@gmail.com>
2021-02-10 01:06:09 +0000

    589. t.1. [Init commit].

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,8 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastNname: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience: numbe;
+  location: string;
+  [key: string: any;
+}
