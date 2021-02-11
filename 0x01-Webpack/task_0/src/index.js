1c7898e3c273f59cb01c2c563ca7cf0d091c7edf
jfkquality <jfkquality@gmail.com>
2021-02-11 20:54:51 +0000

    581. t.0. [Bugfix]. Use createElement('p'), .append.'

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -3,7 +3,17 @@ import $ from "jquery";
 // Add 3 <p>'s to the page body
 
 $(document).ready(() => {
-  $('p').append('Holberton Dashboard');
-  $('p').append('Dashboard data for the students');
-  $('p').append('Copyright - Holberton School');
+  let p1 = document.createElement('p');
+  let p2 = document.createElement('p');
+  let p3 = document.createElement('p');
+
+  p1.innerHTML =('Holberton Dashboard');
+  p2.innerHTML = ('Dashboard data for the students');
+  p3.innerHTML = ('Copyright - Holberton School');
+
+  $('body').append(p1, p2, p3);
+
+  // $('p').append('Holberton Dashboard');
+  // $('p').append('Dashboard data for the students');
+  // $('p').append('Copyright - Holberton School');
 });
