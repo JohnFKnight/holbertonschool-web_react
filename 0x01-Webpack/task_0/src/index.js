965cacbbec76af79e044e3a04e58db1546f44183
jfkquality <jfkquality@gmail.com>
2021-02-11 20:58:48 +0000

    581. t.0. [Bugfix]. Remove () around innerHTML text.

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -7,9 +7,9 @@ $(document).ready(() => {
   let p2 = document.createElement('p');
   let p3 = document.createElement('p');
 
-  p1.innerHTML =('Holberton Dashboard');
-  p2.innerHTML = ('Dashboard data for the students');
-  p3.innerHTML = ('Copyright - Holberton School');
+  p1.innerHTML ='Holberton Dashboard';
+  p2.innerHTML = 'Dashboard data for the students';
+  p3.innerHTML = 'Copyright - Holberton School';
 
   $('body').append(p1, p2, p3);
 
