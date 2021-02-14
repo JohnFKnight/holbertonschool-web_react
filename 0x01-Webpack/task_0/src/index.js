b0c018b3b486e0dad0d6900b9631c1363403ea81
jfkquality <jfkquality@gmail.com>
2021-02-13 22:44:53 -0500

    581. t.0. [Bugfix]. User .append not .append.

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -3,15 +3,20 @@ import $ from "jquery";
 // Add 3 <p>'s to the page body
 
 $(document).ready(() => {
-  let p1 = document.createElement('p');
-  let p2 = document.createElement('p');
-  let p3 = document.createElement('p');
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
 
-  p1.innerHTML ='Holberton Dashboard';
-  p2.innerHTML = 'Dashboard data for the students';
-  p3.innerHTML = 'Copyright - Holberton School';
 
-  $('body').append(p1, p2, p3);
+  // let p1 = document.createElement('p');
+  // let p2 = document.createElement('p');
+  // let p3 = document.createElement('p');
+
+  // p1.innerHTML ='Holberton Dashboard';
+  // p2.innerHTML = 'Dashboard data for the students';
+  // p3.innerHTML = 'Copyright - Holberton School';
+
+  // $('body').append(p1, p2, p3);
 
   // $('p').append('Holberton Dashboard');
   // $('p').append('Dashboard data for the students');
