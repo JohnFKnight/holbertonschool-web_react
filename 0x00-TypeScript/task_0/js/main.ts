69149ba3c4357c1694172e477cba32febb22849a
jfkquality <jfkquality@gmail.com>
2021-02-14 23:24:06 -0500

    589. t.3. [Bugfix]. Use DOM element types.

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -19,19 +19,19 @@ let student2: Student = {
     location: 'Los Angeles',
 }
 
-const studentsList: any = [student1, student2];
-let table: any = document.createElement('table');
+const studentsList: Array<Student> = [student1, student2];
+let table: HTMLTableElement = document.createElement('table');
 document.body.appendChild(table);
 
 for (let student: Student of studentsList ) {
     // console.log('STUDENT: ', student);
-    let row: any = table.insertRow();
-        console.log(student.firstName);
-	console.log(student.location);
-    	let td: any = row.insertCell();
+    let row: HTMLTableRowElement = table.insertRow();
+        // console.log(student.firstName);
+	// console.log(student.location);
+    	let td: HTMLTableCellElement = row.insertCell();
 	let text: string = document.createTextNode(student.firstName);
 	td.appendChild(text);
-    	let td: any = row.insertCell();
+    	let td: HTMLTableCellElement = row.insertCell();
 	let text: string = document.createTextNode(student.location);
 	td.appendChild(text);
 }
