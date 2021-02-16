2c99a617d04624c972377c10f2418690f1ff0c4f
jfkquality <jfkquality@gmail.com>
2021-02-16 00:03:10 -0500

    581. t.1. [Update]. Add webpack config file.

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,37 +1,29 @@
 import $ from 'jquery';
 
 $(document).ready(() => {
-  // let p1 = document.createElement('p');
-  // let p2 = document.createElement('p');
-  // let p3 = document.createElement('p');
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<buttonClick here to get starteds</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
 
-  // p1.innerHTML ='Holberton Dashboard';
-  // p2.innerHTML = 'Dashboard data for the students';
-  // p3.innerHTML = 'Copyright - Holberton School';
+  // let button = $(document.createElement('button')).prop({
+  //   type: 'button',
+  //   innerHTML: 'Click here to get started'
+  // })
+  // $('body').append(button);
 
-  // $('body').append(p1, p2, p3);
+  // $('p').append('<p id='count'></p>');
+  // $('p').append('Copyright - Holberton School');
 
-  $('p').append('Holberton Dashboard');
-  $('p').append('Dashboard data for the students');
-  $('p').append('Copyright - Holberton School');
-
-  let button = $(document.createElement('button')).prop({
-    type: 'button',
-    innerHTML: 'Click here to get started'
-  })
-  $('body').append(button);
-
-  $('p').append('<p id='count'></p>');
-  $('p').append('Copyright - Holberton School');
-
+  let count = 0;
   function updateCounter() {
-    let count = 0;
-    $('button').click(() => {
+    $('button').click($.debounce(() => {
       count++;
-      $('#count').text(`{ count } clicks on the button`);
+      $('#count').text(count + ' clicks on the button');
       // $('#count').html((i, val) {
       // 	return val * 1 + 1;
       // })
-    })
+    }, 1000));
   }
 });
---
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  entry: "./js/dashboard_main.js",
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  }
+};
