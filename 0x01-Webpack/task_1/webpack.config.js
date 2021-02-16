97f6b15668bebf86d6baacc843c00330b16fa0c5
jfkquality <jfkquality@gmail.com>
2021-02-16 01:49:06 -0500

    581. t.1. [Refactor]. Delete commented lines.

--- 0x01-Webpack/task_1/webpack.config.js
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -1,17 +1,8 @@
 const path = require("path");
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
   entry: "./js/dashboard_main.js",
   mode: "production",
-  // plugins: [
-  //   new CleanWebpackPlugin(),
-  //   new HtmlWebpackPlugin({
-  //     title: "Production"
-  //   })
-  // ],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public")
