e29b3ae67901de4c1eb0d4dfc5a6d56e12995c24
jfkquality <jfkquality@gmail.com>
2021-02-19 22:29:17 -0500

    596. t.0-1. Use Holberton favicon.

---
+++ 0x02-react_intro/task_2/dashboard/src/utils.js
@@ -0,0 +1,14 @@
+export function getFullYear() {
+    const d = new Date();
+    return d.getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+    if (isIndex) {
+        return 'Holberton School';
+    } else {
+        return 'Holberton School main dashboard';
+    }
+}
+
+// export {getFullYear, getFooterCopy}  
