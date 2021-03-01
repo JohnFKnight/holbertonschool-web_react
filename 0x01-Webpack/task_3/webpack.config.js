29046495754fdf811271e2c256c9e566886e673d
jfkquality <jfkquality@gmail.com>
2021-02-28 22:22:43 -0500

    581. t.3. [Bugfix]. Change script to 'web-dev-server' but doesnt work. s/b 'webpack serve'.

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   mode: "development",
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, 'public'),
     compress: true,
     port: 8564,
   },
