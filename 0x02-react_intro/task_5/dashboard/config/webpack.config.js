8a943f7ecf574ff786369ffda8a695b5b0f5617d
jfkquality <jfkquality@gmail.com>
2021-03-09 02:06:51 -0500

    596. t.8/task_5. [Bugfix]. Change config, json to account for directory structure.

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: '../src/index.js',
   mode: "development",
   devServer: {
-    contentBase: path.join(__dirname, './dist'),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 8564,
   },
