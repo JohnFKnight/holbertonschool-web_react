8fc36165a854d3d8051036f8de9331327ab7c468
jfkquality <jfkquality@gmail.com>
2021-03-09 01:50:31 -0500

    596. t.8/task_5. [Bugfix]. Restore package.json, webpack config. CREATE REQUIRED FOLDERS.

---
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  entry: './src/index.js',
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    compress: true,
+    port: 8564,
+  },
+  module: {
+    rules: [
+      {
+       test: /\.css$/i,
+       use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devtool: 'inline-source-map',
+};
