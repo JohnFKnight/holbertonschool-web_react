5faae0ef6187a31d1fdf9accf4b697fa291e9b73
jfkquality <jfkquality@gmail.com>
2021-03-16 03:16:10 -0400

    506. t.11. [Init commit].

--- 0x02-react_intro/task_5/dashboard/src/App/App.test.js
+++ 0x02-react_intro/task_5/dashboard/src/App/App.test.js
@@ -5,19 +5,20 @@ import { shallow, mount, render } from 'enzyme';
 
 // configure({adapter: new Adapter()});
 const wrapper = shallow(<App />);
-it('renders without crashing', () => {
-    shallow(<App />);
+describe('<App />', () => {
+    it('renders without crashing', () => {
+      expect(wrapper.exists());
+    });
+    it('renders App-header div', () => {
+      expect(wrapper.contains(<header className="App-header" />))
+    });
+    it('renders App-body div', () => {
+      expect(wrapper.contains(<main className="App-body" />))
+    });
+    it('renders App-footer div', () => {
+      expect(wrapper.contains(<footer className="App-header" />))
+    });
 });
-it('renders a div with the class App-header', () => {
-    expect(wrapper.find("div.App-header").length).toBe(1);
-});
-it('renders a div with the class App-body', () => {
-    expect(wrapper.find("div.App-body").length).toBe(1);
-});
-it('renders a div with the class App-footer', () => {
-    expect(wrapper.find("div.App-footer").length).toBe(1);
-});
-
 // These also work but don't show as test counts
 // expect(wrapper.find("div.App-header").length).toBe(1);
 // expect(wrapper.find("div.App-body").length).toBe(1);
