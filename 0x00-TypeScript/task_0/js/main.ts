029cd7328855f18cbc3294277b306c2fbbada6ec
jfkquality <jfkquality@gmail.com>
2021-04-05 01:40:33 -0400

    589. t.0. [Refactor]. Dont have to append when using insert.

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -1,49 +1,92 @@
+'use strict';
 interface Student {
   firstName: string;
   lastName: string;
   age: number;
   location: string;
 }
-const student1: Student = {
-  firstName: "Betty",
-  lastName: "Holberton",
-  age: 44,
-  location: "USA",
+let student1: Student = {
+    firstName: 'Joe',
+    lastName: 'Schmoe',
+    age: 25,
+    location: 'Hollywood',
 }
-const student2: Student = {
-  firstName: "Jhon",
-  lastName: "Parker",
-  age: 52,
-  location: "Canada",
+
+let student2: Student = {
+    firstName: 'Jane',
+    lastName: 'Schmane',
+    age: 23,
+    location: 'Los Angeles',
 }
-// Create array o type Student
+
 const studentsList: Array<Student> = [student1, student2];
-
-const table: HTMLTableElement = document.createElement('table');
+// let body: HTMLBodyElement = document.getElementsByTagName("body")[0];
+const tbl: HTMLTableElement = document.createElement('table');
 const thead: HTMLTableSectionElement = document.createElement('thead');
-const tbody: HTMLTableSectionElement = document.createElement('tbody');
-const trHead: HTMLTableRowElement = document.createElement('tr');
-const th1: HTMLTableHeaderCellElement = document.createElement('th');
-const th2: HTMLTableHeaderCellElement = document.createElement('th');
+const tblBody: HTMLTableSectionElement = document.createElement("tbody");
+const trHead: HTMLTableRowElement = thead.insertRow();
+const th1: HTMLTableCellElement = trHead.insertCell();
+const th2: HTMLTableCellElement = trHead.insertCell();
 
-th1.innerHTML = 'firstName';
-th2.innerHTML = 'location';
+th1.innerHTML = "Name";
+th2.innerHTML = "Location";
 
-table.append(thead);
-table.append(tbody);
-thead.append(trHead);
-trHead.append(th1);
-trHead.append(th2);
+tbl.append(thead);
+// thead.append(trHead);
+// trHead.append(th1);
+// trHead.append(th2);
 
 studentsList.forEach((student) => {
-    const row: HTMLTableRowElement = table.insertRow();
-    tbody.append(row);
-    const cell: HTMLTableCellElement = row.insertCell();
-    const text: Text = document.createTextNode(student.firstName);
-    cell.appendChild(text);
-
-    const cell2: HTMLTableCellElement = row.insertCell();
+// for (const student of studentsList ) {
+    const row: HTMLTableRowElement = tbl.insertRow();
+    const td1: HTMLTableCellElement = row.insertCell();
+    const text1: Text = document.createTextNode(student.firstName);
+    td1.appendChild(text1);
+    // let text: string = document.createTextNode(student.firstName);
+    // td.appendChild(text);
+    const td2: HTMLTableCellElement = row.insertCell();
     const text2: Text = document.createTextNode(student.location);
-    cell2.appendChild(text2);
+    td2.appendChild(text2);
+    // let text: string = document.createTextNode(student.location);
+    // td.appendChild(text);
+    tblBody.appendChild(row);
 })
-document.body.appendChild(table)
+
+tbl.append(tblBody);
+document.body.appendChild(tbl);
+
+// function generateTableHead(table, data) {
+//  let thead: HTMLTableSectionElement = table.createTHead(table);
+//  let row: HTMLTableRowElement = thead.insertRow();
+//  for(let key: string of data) {
+//    let th: string = document.createElement('th');
+//    let text: string = document.createTextNode(key);
+//    th.appendChild(text);
+//    row.appendChid(th);
+//    }
+// }
+
+// function generateTable(table, data) {
+//   for (let element: string of data) {
+//     let row: string = table.insertRow();
+//     for (let key: string in element) {
+//       let cell: string = row.insertCell();
+//       let text: string  = document.createTextNode(element[key]);
+//       cell.appendChild(text)
+//     }
+//   }
+// }
+
+// let table: HTMLTableElement = document.createElement('table');
+// let data: Array<string> = Object.keys(studentsList[0]);
+// let p: HTMLParagraphElement = document.createElement('p');
+// const fname: String = data[0];
+// console.log("fname", fname);
+// p.textContent = studentsList[1][{ fname }];
+// //p.textContent = studentsList[1].firstName;
+// console.log("p.textContent", p.textContent);
+// document.body.appendChild(p);
+// // table.appendChild(document.createTextNode(data));
+// console.log("data", data);
+// generateTable(table, studentsList);
+// generateTableHead(table,data);
