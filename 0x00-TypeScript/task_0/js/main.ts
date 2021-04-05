24c1ccc27d01b1da5577a764f565ac1ecad705a7
jfkquality <jfkquality@gmail.com>
2021-04-04 23:54:23 -0400

    589. t.0. [Bugfix]. Actually create a table; TS every variable.

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -19,24 +19,27 @@ let student2: Student = {
     location: 'Los Angeles',
 }
 
-const studentsList: Array<Student> = [student1, student2];
-let table: HTMLTableElement = document.createElement('table');
-document.body.appendChild(table);
+let studentsList: Array<Student> = [student1, student2];
+// let body: HTMLBodyElement = document.getElementsByTagName("body")[0];
+let tbl: HTMLTableElement = document.createElement('table');
+let tblBody: HTMLTableSectionElement = document.createElement("tbody");
+document.body.appendChild(tbl);
+tbl.appendChild(tblBody);
 
-// for (const student of studentsList ) {
-//     // console.log('STUDENT: ', student);
-//     let row: HTMLTableRowElement = table.insertRow();
-//     // console.log(student.firstName);
-//     // console.log(student.location);
-//     const td1: HTMLTableCellElement = row.insertCell();
-//     td1.textContent = student.firstName;
-//     // let text: string = document.createTextNode(student.firstName);
-//     // td.appendChild(text);
-//     const td2: HTMLTableCellElement = row.insertCell();
-//     td2.textContent = student.location;
-//     // let text: string = document.createTextNode(student.location);
-//     // td.appendChild(text);
-// }
+for (const student of studentsList ) {
+    // console.log('STUDENT: ', student);
+    let row: HTMLTableRowElement = tbl.insertRow();
+    // console.log(student.firstName);
+    // console.log(student.location);
+    const td1: HTMLTableCellElement = row.insertCell();
+    td1.textContent = student.firstName;
+    // let text: string = document.createTextNode(student.firstName);
+    // td.appendChild(text);
+    const td2: HTMLTableCellElement = row.insertCell();
+    td2.textContent = student.location;
+    // let text: string = document.createTextNode(student.location);
+    // td.appendChild(text);
+}
 
 // function generateTableHead(table, data) {
 //  let thead: HTMLTableSectionElement = table.createTHead(table);
@@ -60,16 +63,16 @@ document.body.appendChild(table);
 //   }
 // }
 
-let table: HTMLTableElement = document.createElement('table');
-let data: array = Object.keys(studentsList[0]);
-let p = document.createElement('p');
-const name = data[0];
-console.log(name);
-p.textContent = studentsList[1][name];
-//p.textContent = studentsList[1].firstName;
-console.log(p.textContent);
-document.body.appendChild(p);
-// table.appendChild(document.createTextNode(data));
-console.log(data);
+// let table: HTMLTableElement = document.createElement('table');
+// let data: Array<string> = Object.keys(studentsList[0]);
+// let p: HTMLParagraphElement = document.createElement('p');
+// const fname: String = data[0];
+// console.log("fname", fname);
+// p.textContent = studentsList[1][{ fname }];
+// //p.textContent = studentsList[1].firstName;
+// console.log("p.textContent", p.textContent);
+// document.body.appendChild(p);
+// // table.appendChild(document.createTextNode(data));
+// console.log("data", data);
 // generateTable(table, studentsList);
 // generateTableHead(table,data);
