91a8a6a4b426272d81d51c42c8511b739e90beb7
jfkquality <jfkquality@gmail.com>
2021-05-10 19:26:36 -0400

    589. t.4/task_1. [Bugfix]. Try different code for check 2.

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -53,33 +53,31 @@ console.log(teach('John', 'Knight'));
 // Task 4.
 
 interface StudentConstructor {
-  new (firstName: string, lastName: string): StudentClassInterface;
+  new(firstName: string, lastName: string): StudentClassInterface;
 }
-
+  
 interface StudentClassInterface {
-  firstName: string;
-  lastName: string;
-  displayName(): string;
   workOnHomework(): string;
+  displayName(): string;
 }
-
+  
 export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
   firstName: string;
   lastName: string;
-
+  
   constructor(firstName: string, lastName: string) {
     this.firstName = firstName;
     this.lastName = lastName;
   }
-
+  
   workOnHomework(): string {
-    return 'Currently working';
+    return "Currently working";
   }
-
+  
   displayName(): string {
     return this.firstName;
   }
-};
+}
 
 const student = new StudentClass('Guillaume', 'Salva');
 console.log(student.firstName);
