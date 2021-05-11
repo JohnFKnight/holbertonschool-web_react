a1af6fa2e7e780a4f95c325cfda81d0b89626d60
jfkquality <jfkquality@gmail.com>
2021-05-11 18:53:48 -0400

    589. t-5/taks_2. [Bugfix]. export classes.

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -14,7 +14,7 @@ interface TeacherInterface {
   workTeacherTasks(): string;
 }
 
-class Director implements DirectorInterface {
+export class Director implements DirectorInterface {
   workFromHome(): string {
     return 'Working from home';
   }
@@ -28,7 +28,7 @@ class Director implements DirectorInterface {
   }
 }
 
-class Teacher implements TeacherInterface {
+export class Teacher implements TeacherInterface {
   workFromHome(): string {
     return 'Cannot work from home';
   }
