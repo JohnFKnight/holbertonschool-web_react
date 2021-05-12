c0ff4d4a30706c039b708ec06551893af73e3ac1
jfkquality <jfkquality@gmail.com>
2021-05-11 23:49:35 -0400

    589. t-7/taks_2. [Bugfix]. Set todayClass type to Subjects.

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -85,7 +85,7 @@ console.log(executeWork(createEmployee(1000)));
 
 type Subjects = "Math" | "History";
 
-export function teachClass(todayClass: string) {
+export function teachClass(todayClass: Subjects) {
   if (todayClass === 'Math') {
     return 'Teaching Math';
   }
