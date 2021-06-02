5ece89a73aa3330508c7d3eeb0714b4dbe885e4e
jfkquality <jfkquality@gmail.com>
2021-06-02 03:21:08 -0400

    589. t-8/task_3. [Bugfix] Comment out console.log(new row id).

--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -9,7 +9,7 @@ const row: RowElement = {
 }
 
 const newRowID: RowID = CRUD.insertRow(row);
-console.log(newRowID);
+// console.log(newRowID);
 const updatedRow: RowElement = { ...row, age: 23 };
 
 CRUD.updateRow(newRowID, updatedRow);
